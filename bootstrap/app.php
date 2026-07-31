b43afab31dd53d9c780c699b6dde8b703f95cd34
KanavMahendru <gbytedeveloper@gmail.com>
2026-07-31 17:46:21 +0530

    changes in active shop

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+        Route::middleware('web')
+            ->group(base_path('routes/ai.php'));
+        },
     )
     ->withSchedule(function (Schedule $schedule): void {
 
