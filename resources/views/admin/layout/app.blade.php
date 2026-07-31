61dafba133c2ac03e79ff90bb36247a04c51b0ca
KanavMahendru <gbytedeveloper@gmail.com>
2026-07-31 19:16:04 +0530

    Added Logo to site

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -6,8 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- Bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logo/favamzsync.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('logo/favamzsync.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('logo/favamzsync.png') }}">
+
     <style>
         body {
             background: #f5f7fb;
