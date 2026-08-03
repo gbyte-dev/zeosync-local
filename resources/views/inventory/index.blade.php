d9604df76dca5f356bc8eb59ebecec50bb575397
KanavMahendru <gbytedeveloper@gmail.com>
2026-08-03 18:40:59 +0530

    iframe view updated

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -51,6 +51,10 @@
         min-width: 280px;
     }
 
+    .in-iframe .saas-usage-box {
+        padding: 0px 0px;
+    }
+
     .saas-usage-header {
         display: flex;
         justify-content: space-between;
