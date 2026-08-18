f3507368193ea68a0c75d083396e742b1559d080
Programmar-Brijesh <brijeshverma8896@gmail.com>
2026-08-18 13:23:19 +0530

    fixed notification read issue

--- routes/webnotification.php
+++ routes/webnotification.php
@@ -26,5 +26,10 @@ Route::prefix('admin')->group(function () {
 Route::post('/user/notification/{id}/read', [NotificationController::class, 'markUserNotificationRead'])
     ->name('user.notification.read');
 
+Route::post(
+    '/user/notifications/mark-viewed',
+    [NotificationController::class, 'markViewedUserNotificationsRead']
+)->name('user.notifications.markViewed');
+
 /* Route::post('/user/notification/mark-all-read', [NotificationController::class, 'markAllUserNotificationsRead'])
     ->name('user.notification.markAllRead'); */
