9d9f5daa6a25f84195cbd00688dd28549f5fbd98
Programmar-Brijesh <brijeshverma8896@gmail.com>
2026-08-22 19:14:55 +0530

    added fixes

--- app/Services/TransformsAmazonAttributes.php
+++ app/Services/TransformsAmazonAttributes.php
@@ -1245,10 +1245,6 @@ class TransformsAmazonAttributes
                 return $this->closure($value);
             }
 
-            if ($name === 'esrb_rating') {
-                return $this->esrbRating($value, $marketplaceId);
-            }
-
             if ($name === 'light_source') {
                 return $this->lightSource($value, $marketplaceId);
             }
@@ -1391,19 +1387,6 @@ class TransformsAmazonAttributes
         ]];
     }
 
-    private function esrbRating($value, $marketplaceId): array
-    {
-        return [[
-            'rating' => [[
-                'value' => $value['rating'],
-            ]],
-            'descriptors' => array_map(
-                fn($descriptor) => ['value' => $descriptor],
-                $value['descriptors']
-            ),
-            'marketplace_id' => $marketplaceId,
-        ]];
-    }
     // lightsource 
     private function lightSource($value, $marketplaceId = null): array
     {
