f88788161af4ec9c5123abd3b675f30976add165
KanavMahendru <gbytedeveloper@gmail.com>
2026-09-14 15:38:16 +0530

    changes in view

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -427,13 +427,13 @@
             </a>
             <a href="{{ route('admin.contact-requests') }}"
                 class="sidebar-link {{ request()->routeIs('admin.contact-requests*') ? 'active' : '' }}">
-                <i class="bi bi-chat-left-text"></i> Contact Requests
+                <i class="bi bi-chat-left-text"></i> Contact Requests <span class="badge bg-danger rounded-pill ms-auto">{{ getContactInquiryUnread()->count() }}</span>
             </a>
             <a href="{{ route('admin.plans') }}" class="sidebar-link">
                 <i class="bi bi-credit-card"></i> Plans
             </a>
             <a href="{{ route('admin.notification') }}" class="sidebar-link">
-                <i class="bi bi-bell"></i> Notification
+                <i class="bi bi-bell"></i> Notification <span class="badge bg-danger rounded-pill ms-auto">{{ getAdminNotificationUnread()->count() }}</span>
             </a>
             <a href="{{ route('admin.settings') }}" class="sidebar-link">
                 <i class="bi bi-gear"></i> Settings
