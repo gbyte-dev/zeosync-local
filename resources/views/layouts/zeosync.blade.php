f680b539d3fb279df60b42420e21e377060e0d0b
KanavMahendru <gbytedeveloper@gmail.com>
2026-09-16 11:37:32 +0530

    changes in data to check

--- resources/views/layouts/zeosync.blade.php
+++ resources/views/layouts/zeosync.blade.php
@@ -20,8 +20,8 @@
     <title>@yield('title', config('app.name', 'Zeosync'))</title>
     <meta name="description" content="@yield('meta_description', 'Connect Amazon and Shopify with clearer product, inventory, order and returns workflows.')">
     <meta name="theme-color" content="#111c25">
-    <meta name="shopify-api-key" content="{{ $shopifyclient_id }}">
-    <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+ {{--   <meta name="shopify-api-key" content="{{ $shopifyclient_id }}">
+    <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script> --}}
     <link rel="icon" type="image/png" sizes="32x32" href="{{ $faviconUrl }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ $faviconUrl }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ $faviconUrl }}">
