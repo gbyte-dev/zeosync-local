1f2f7babfd7842573b0587bef73829bca6fd3c8e
Programmar-Brijesh <brijeshverma8896@gmail.com>
2026-09-18 19:25:06 +0530

    added nonce in all blade file in script and style tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link nonce="{{ $cspNonce??'' }}" href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link nonce="{{ $cspNonce??'' }}" nonce="{{ $cspNonce??'' }}"href="{{ asset('css/sidebar.css') }}?v={{ time() }}" rel="stylesheet">
+    <link nonce="{{ $cspNonce??'' }}" href="{{ asset('css/sidebar.css') }}?v={{ time() }}" rel="stylesheet">
     <link nonce="{{ $cspNonce??'' }}" href="{{ asset('css/style.css') }}?v={{ time() }}" rel="stylesheet">
     @php
     $favicon = \App\Models\AdminSetting::where('option_key', 'app_favicon')->value('option_value');
